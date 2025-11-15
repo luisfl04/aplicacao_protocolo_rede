acb76c4831ee85eefecb6ba6b940ab7e4be88531
luisfl04 <luisfelipecontato08@gmail.com>
2025-11-15 15:35:10 -0300

    feat: Adiciona uso de variáveis de ambiente na aplicação

--- cliente/pacote.py
+++ cliente/pacote.py
@@ -1,10 +1,16 @@
 import struct # Para criação do pacote
 import zlib # Para checksum
 import logging
-
-
+from decouple import config
 
 class Packet:
+
+    HEADER_FORMAT = config("HEADER_FORMAT")
+    HEADER_SIZE = struct.calcsize(HEADER_FORMAT)
+    FLAG_SYN = config("FLAG_SYN") 
+    FLAG_ACK = config("FLAG_ACK")  
+    FLAG_CHECKSUM = config("FLAG_CHECKSUM")
+
     def __init__(self, sequence_number=0, ack_number=0, flags=0, data=b''):
         self.sequence_number = sequence_number
         self.ack_number = ack_number
@@ -12,28 +18,6 @@ class Packet:
         self.checksum = 0
         self.data = data
     
-    def get_package_properties(self) -> dict | None:
-        """
-        Retorna as propiedades necessesárias pra criar o pacote
-        """
-        try:    
-            HEADER_FORMAT = '!IIHH'
-            HEADER_SIZE = struct.calcsize(HEADER_FORMAT)
-            FLAG_SYN = 1 << 0  
-            FLAG_ACK = 1 << 1  
-            FLAG_CHECKSUM = 1 << 2
-            propiedades = {
-                "header_format": HEADER_FORMAT,
-                "header_size": HEADER_SIZE,
-                "flag_syncronizate": FLAG_SYN,
-                "flag_ack": FLAG_ACK,
-                "flag_checksum": FLAG_CHECKSUM
-            }
-            return propiedades
-        except Exception as e:
-            logging.error(f"Erro ao obter propiedades do pacote: {e}")
-            return None
-
     def pack_package(self) -> bytes | None:
         """
             Função usada para empacotar o pacote, fazendo a junção do cabeçalho com os dados
@@ -41,9 +25,9 @@ class Packet:
 
         try:
             # Empacotando versão inicial do cabeçalho:
-            propiedades = self.get_package_properties()
+            propriedades = self.get_package_properties()
             header = struct.pack(   
-                propiedades.get("header_format"),
+                self.HEADER_FORMAT,
                 self.sequence_number,
                 self.ack_number,
                 self.flags,
@@ -54,7 +38,7 @@ class Packet:
             self.checksum = zlib.crc32(header + self.data) & 0xffff
 
             # Empacotando novamente o cabeçalho com o checksum: 
-            header_with_checksum = struct.pack(propiedades.get("header_format"), 
+            header_with_checksum = struct.pack(self.HEADER_FORMAT, 
                                             self.sequence_number, 
                                             self.ack_number, 
                                             self.flags, 
@@ -75,15 +59,12 @@ class Packet:
         """
 
         try:
-            # Obtendo propriedades do pacote:
-            propiedades = self.get_package_properties()
-
             # Filtrando cabeçalho e dados:
-            header_data = raw_data[:propiedades.get("header_size")]
-            data = raw_data[propiedades.get("header_size"):]
+            header_data = raw_data[:self.HEADER_SIZE]
+            data = raw_data[self.HEADER_SIZE:]
             
             # Desempacotando o cabeçalho:
-            sequence_number, ack_number, flags, checksum_received = struct.unpack(propiedades.get("header_format"), header_data)
+            sequence_number, ack_number, flags, checksum_received = struct.unpack(self.HEADER_FORMAT, header_data)
             
             # Criando o objeto packet e armazenando o checksum:
             packet = Packet(sequence_number, ack_number, flags, data)
@@ -95,7 +76,13 @@ class Packet:
             return None
 
     def __str__(self):
-        """ Representação em string para fácil depuração (debug). """    
-        return (f"[Packet: SEQ={self.seq_num} ACK={self.ack_num} "
+        """ Representação em string para fácil depuração (debug). """
+        flag_str = []
+        if self.flags & self.FLAG_SYN: flag_str.append("SYN")
+        if self.flags & self.FLAG_ACK: flag_str.append("ACK")
+        if self.flags & self.FLAG_CHECKSUM: flag_str.append("FIN")
+        if not flag_str: flag_str.append("DATA")
+        
+        return (f"[Packet: SEQ={self.sequence_number} ACK={self.ack_number} "
                 f"Flags={'|'.join(flag_str)} Checksum={self.checksum} "
                 f"DataLen={len(self.data)}]")
